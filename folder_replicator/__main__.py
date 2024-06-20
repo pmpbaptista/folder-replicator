6efc593ed7302b5be3c6cd00d3d4b614303f864e
Pedro Baptista <32106544+pmpbaptista@users.noreply.github.com>
2024-06-20 19:02:26 +0000

    docs: fix docs workflow
    Docs workflow was failing because of incompatible versions.
    
    Issue: none

--- folder_replicator/__main__.py
+++ folder_replicator/__main__.py
@@ -1,6 +1,14 @@
 """ Main module for the folder-replicator package. """
 
 def main():
+    """
+    Main function for the folder-replicator package.
+    args:
+        None
+    returns:
+        None
+    """
+
     print("Hello, world!")
 
 if __name__ == "__main__":
